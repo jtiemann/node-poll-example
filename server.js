481e9d8d4311889ca0dd46f3568f33d49daec1bf
Jon Tiemann <jtiemann@digitalpersonae.com>
2014-03-14 10:01:33 -0400

    initail commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 var connect = require('connect')
     , express = require('express')
     , io = require('socket.io')
-    , port = (process.env.PORT || 8081);
+    , port = (process.env.PORT || 4001);
 
 //Setup Express
 var server = express.createServer();
@@ -39,12 +39,117 @@ server.listen( port);
 
 //Setup Socket.IO
 var io = io.listen(server);
+var answerArr = [],// array of yes, no answers for each question [3, 5] is 3 yeses and 5 nos for question name 0
+    voter = {},//contains voter.address, voter.votes, an object containing an array of answers indexed by question number (name)
+    voterAddressArr = [], //an array of voter ip's'
+    voterArr = []; // array of voter objects
+;
+var likesArr = [],// array of yes, no answers for each question [3, 5] is 3 yeses and 5 nos for question name 0
+  liker = {},//contains voter.address, voter.votes, an object containing an array of answers indexed by question number (name)
+  likerAddressArr = [], //an array of voter ip's'
+  likerArr = []; // array of voter objects
+
 io.sockets.on('connection', function(socket){
   console.log('Client Connected');
+
   socket.on('message', function(data){
-    socket.broadcast.emit('server_message',data);
-    socket.emit('server_message',data);
+    /*
+    get this connections address (ip)
+    look in voterAddressArr to see if in there (if not add it)
+    check if user has answered this question in voterArr[idx].votes array
+    (idx in voterAddressArr is used as entry pointer in voterArr)
+    if not in voterArr[idx].votes, then vote and add to votes, else just return
+
+     */
+    //console.log(socket)
+    var address = socket.handshake.address;
+    console.log("New connection from " + address.address + ":" + address.port);
+    //check for presence
+    var idx = voterAddressArr.indexOf(address.address);
+    if ( idx !== -1) {
+      voter = voterArr[idx] || {"address": address.address, "votes": []};
+    }
+    else {
+      voterAddressArr.push(address.address);
+      voter = voterArr[voterAddressArr.length-1] = {"address": address.address, "votes": []};
+    }
+
+    var vote = JSON.parse(data); //data.name = question number, data.answer = answer
+    var arrIndex = vote.answer === 'yes' ? parseInt(vote.name) * 2 : parseInt(vote.name) * 2 + 1
+
+    if (!voter.votes[vote.name]) {
+      voter.votes[vote.name] = vote.answer
+      answerArr[arrIndex] = answerArr[arrIndex] + 1 || 1;
+      var jsonResponse = JSON.stringify({
+        "questionNumber": vote.name,
+        "answer": vote.answer,
+        "yesCount": answerArr[parseInt(vote.name)*2] || 0,
+        "noCount": answerArr[parseInt(vote.name)*2+1] || 0
+      })
+      socket.broadcast.emit('server_message',jsonResponse);
+      socket.emit('server_message',jsonResponse);
+    }
+
+//    var jsonResponse = JSON.stringify({
+//      "questionNumber": vote.name,
+//      "answer": vote.answer,
+//      "yesCount": answerArr[parseInt(vote.name)*2] || 0,
+//      "noCount": answerArr[parseInt(vote.name)*2+1] || 0
+//    })
+//    socket.broadcast.emit('server_message',jsonResponse);
+//    socket.emit('server_message',jsonResponse);
+
   });
+
+  socket.on('like', function(data){
+    /*
+     get this connections address (ip)
+     look in voterAddressArr to see if in there (if not add it)
+     check if user has answered this question in voterArr[idx].votes array
+     (idx in voterAddressArr is used as entry pointer in voterArr)
+     if not in voterArr[idx].votes, then vote and add to votes, else just return
+
+     */
+    //console.log(socket)
+    var address = socket.handshake.address;
+    console.log("New connection from " + address.address + ":" + address.port);
+    //check for presence
+    var idx = likerAddressArr.indexOf(address.address);
+    if ( idx !== -1) {
+      liker = likerArr[idx] || {"address": address.address, "votes": []};
+    }
+    else {
+      likerAddressArr.push(address.address);
+      liker = likerArr[likerAddressArr.length-1] = {"address": address.address, "votes": []};
+    }
+
+    var vote = JSON.parse(data); //data.name = like number, data.answer = answer
+    var arrIndex = vote.answer === 'like' ? parseInt(vote.name) * 2 : parseInt(vote.name) * 2 + 1
+
+    //if (!liker.votes[vote.name]) {
+      liker.votes[vote.name] = vote.answer
+      likesArr[arrIndex] = likesArr[arrIndex] + 1 || 1;
+      var jsonResponse = JSON.stringify({
+        "likeNumber": vote.name,
+        "answer": vote.answer,
+        "likeCount": likesArr[parseInt(vote.name)*2] || 0,
+        "notLikeCount": likesArr[parseInt(vote.name)*2+1] || 0
+      })
+      socket.broadcast.emit('server_like',jsonResponse);
+      socket.emit('server_like',jsonResponse);
+   // }
+
+//    var jsonResponse = JSON.stringify({
+//      "questionNumber": vote.name,
+//      "answer": vote.answer,
+//      "yesCount": answerArr[parseInt(vote.name)*2] || 0,
+//      "noCount": answerArr[parseInt(vote.name)*2+1] || 0
+//    })
+//    socket.broadcast.emit('server_message',jsonResponse);
+//    socket.emit('server_message',jsonResponse);
+
+  });
+  
   socket.on('disconnect', function(){
     console.log('Client Disconnected.');
   });
@@ -58,14 +163,7 @@ io.sockets.on('connection', function(socket){
 /////// ADD ALL YOUR ROUTES HERE  /////////
 
 server.get('/', function(req,res){
-  res.render('index.jade', {
-    locals : { 
-              title : 'Your Page Title'
-             ,description: 'Your Page Description'
-             ,author: 'Your Name'
-             ,analyticssiteid: 'XXXXXXX' 
-            }
-  });
+  res.render('index.html');
 });
 
 
--- static/js/script.js
+++ static/js/script.js
@@ -1,15 +1,155 @@
-/* Author: YOUR NAME HERE
+/* Author: Jon "T-Bone" Tiemann
 */
 
 $(document).ready(function() {   
 
   var socket = io.connect();
+  //var answerArr = [];
 
-  $('#sender').bind('click', function() {
-   socket.emit('message', 'Message Sent on ' + new Date());     
+  $('.sender').bind('click', function(evt) {
+   //alert('thanks for your vote: ' + (evt.currentTarget.value + ' on the question:' + $(this).siblings().first().text()));
+   socket.emit('message', JSON.stringify({"name": evt.currentTarget.name, "answer": evt.currentTarget.value }));
+  });
+
+  $('.like').bind('click', function(evt) {
+    //alert('thanks for your vote: ' + (evt.currentTarget.value + ' on the question:' + $(this).siblings().first().text()));
+    socket.emit('like', JSON.stringify({"name": evt.currentTarget.name, "answer": evt.currentTarget.value }));
   });
 
   socket.on('server_message', function(data){
-   $('#receiver').append('<li>' + data + '</li>');  
+    /* how the server responds
+     var jsonResponse = JSON.stringify({
+     "questionNumber": vote[0],
+     "answer": vote[1],
+     "yesCount": answerArr[parseInt(vote[0])*2] || 0,
+     "noCount": answerArr[parseInt(vote[0])*2+1] || 0
+     })
+     */
+    //parse receiver and add to correct question
+    var vote = JSON.parse(data);
+    var yesPercent = vote.yesCount/(vote.yesCount + vote.noCount);
+    var noPercent = 1 - yesPercent;
+    var buildString = vote.yesCount +  ' Y, ' + vote.noCount + ' N'
+    //$('[name=' + vote.questionNumber + ']').siblings('.result').html(buildString);
+    $('[name=' + vote.questionNumber + ']').siblings('.result').highcharts(barOptions(vote.yesCount, vote.noCount))
+    //$('[name=' + vote.questionNumber + ']').siblings('.result').highcharts(pieOptions(yesPercent, noPercent))
   });
+
+  function pieOptions(yesPercent, noPercent) {
+    return {
+      chart: {
+        //renderTo: 'container',
+        height: '200',
+        type: 'pie',
+        plotBackgroundColor: null,
+        plotBorderWidth: null,
+        plotShadow: false
+      },
+      title: {
+        text: ''
+      },
+      tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+      },
+      plotOptions: {
+        pie: {
+          size: '80%',
+          allowPointSelect: true,
+          cursor: 'pointer',
+          dataLabels: {
+            enabled: true,
+            color: '#000000',
+            connectorColor: '#000000',
+            format: '<b>{point.name}</b>: {point.percentage:.1f} %'
+          }
+        }
+      },
+      series: [{
+        type: 'pie',
+        //name: 'Browser share',
+        data: [
+          ['Yes', yesPercent],
+          ['No', noPercent]
+        ]
+      }]
+    }
+  }
+  function barOptions(yesCount, noCount) {
+  return {
+    chart: {
+      height: '200',
+      width: '400',
+      type: 'column'
+    },
+    title: {
+      text: ''
+    },
+    subtitle: {
+      text: ''
+    },
+    xAxis: {
+      labels: '',
+      title: {
+        text: 'Votes'
+      },
+      categories: [
+        'Yes',
+        'No'
+      ]
+    },
+    yAxis: {
+      min: 0,
+      title: {
+        text: 'Counts'
+      }
+    },
+    tooltip: {enabled:false},
+//    tooltip: {
+//      headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+//      pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+//        '<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
+//      footerFormat: '</table>',
+//      shared: true,
+//      useHTML: true
+//    },
+    plotOptions: {
+      column: {
+        pointPadding: 0.2,
+        borderWidth: 0
+      }
+    },
+    series: [{
+      name: 'Yes',
+      data: [yesCount]
+
+    }, {
+      name: 'No',
+      data: [noCount]
+
+    }]
+  }
+ }
+
+socket.on('server_like', function(data){
+  /* how the server responds
+   var jsonResponse = JSON.stringify({
+   "likeNumber": vote[0],
+   "answer": vote[1],
+   "likeCount": likeArr[parseInt(vote[0])*2] || 0,
+   "notLikeCount": likeArr[parseInt(vote[0])*2+1] || 0
+   })
+   */
+  //parse receiver and add to correct question
+  var vote = JSON.parse(data);
+
+  if (vote.answer === "like"){
+    buildString =  vote.likeCount + ' like';
+    $('[name=' + vote.likeNumber + ']').siblings('.like').first().html(buildString);
+  }
+  else {
+    buildString = vote.notLikeCount + ' not like';
+    $('[name=' + vote.likeNumber + ']').siblings('.like').first().next().html(buildString);
+  }
+});
+
 });
